acc024a721592ea9fd1a42338c0c10ccbb538a7b
WassiW80 <sayedwaseeyat111@gmail.com>
2020-01-27 12:42:09 +0530

    ADDED: Checked resigning condition using while.

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -2,10 +2,12 @@
 echo "Welcome to Gambling Simulator."
 stake=100
 bet=1
-if [ $((RANDOM%2)) -eq 0 ]
-then
-	stake=$(($stake-$bet))
-else
-	stake=$(($stake+$bet))
-fi
-
+while [ $stake -lt 150 ] && [ $stake -gt 50 ]
+do
+	if [ $((RANDOM%2)) -eq 0 ]
+	then
+		stake=$(($stake-$bet))
+	else
+		stake=$(($stake+$bet))
+	fi
+done
