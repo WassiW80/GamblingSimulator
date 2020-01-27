eb0bf4887cc23673f10a4139eb7961174f24ccce
WassiW80 <sayedwaseeyat111@gmail.com>
2020-01-27 13:05:23 +0530

    ADDED: Calculating amount for win or loose After 20 day.

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -2,12 +2,39 @@
 echo "Welcome to Gambling Simulator."
 stake=100
 bet=1
-while [ $stake -lt 150 ] && [ $stake -gt 50 ]
+winningAmount=0
+loosingAmount=0
+for((i=0;i<20;i++))
 do
-	if [ $((RANDOM%2)) -eq 0 ]
+	while [ $stake -lt 150 ] && [ $stake -gt 50 ]
+	do
+		if [ $((RANDOM%2)) -eq 0 ]
+		then
+			stake=$(($stake-$bet))
+		else
+			stake=$(($stake+$bet))
+		fi
+	done
+	if [ $stake -ge 150 ]
 	then
-		stake=$(($stake-$bet))
+		winningAmount=$(($winningAmount+50))
+		stake=100
 	else
-		stake=$(($stake+$bet))
+		loosingAmount=$(($loosingAmount+50))
+		stake=100
 	fi
 done
+
+if [ $winningAmount -gt $loosingAmount ]
+then
+	echo "You won by $(($winningAmount-$loosingAmount)) in 20 day"
+elif [ $winningAmount -eq $loosingAmount ]
+then
+	echo "You neither won nor loose"
+else
+	echo "You loose by $(($loosingAmount-$winningAmount)) in 20 day"
+fi
+
+
+echo $winningAmount
+echo $loosingAmount
